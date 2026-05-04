0cea34b6bf1cd419935662c9769b3632aa52c643
Shaikha Alhammadi <shaikha.saalem@outlook.com>
2026-05-04 12:55:07 +0200

    added files for the application
    Signed-off-by: Shaikha Alhammadi <shaikha.saalem@outlook.com>

--- src/main/scala/utils/Config.scala
+++ src/main/scala/utils/Config.scala
@@ -3,7 +3,7 @@ package utils
 object Config {
 
   // The local directory containing this repository
-  val projectDir :String = "/E:/Git/BigData/lab-cse-25-26"
+  val projectDir :String = "/Users/shaikha/Desktop/UniBo/Big-Data/Steam_Reviews"
   // The name of the shared bucket on AWS S3 to read datasets (so you don't need to upload them in your bucket)
   val s3sharedBucketName :String = "unibo-bd2526-egallinucci-shared"
   // The name of your bucket on AWS S3
